f7da53c52e05ab1b50e17e6eed444ca6fbfe61ad
Ekene <83151271+Yeahx1000@users.noreply.github.com>
2026-01-25 15:02:46 -0500

    init comits pt 3

--- crates/core/main.rs
+++ crates/core/main.rs
@@ -1,3 +1,11 @@
+pub struct metrics {
+    pub point_of_sail: String,
+    pub optimal_sail_angle: f64,
+    pub trim_score: u8,
+    pub speed_factor: f64,
+    pub notes: String,
+}
+
 fn main(){
     println!("Hello, world!");
 }
